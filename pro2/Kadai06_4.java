9b68e85647e4dac72cbfb07d28a33f351ffa1f4e
Ryosuke1041 <rekyuso11@gmail.com>
2023-10-30 20:44:57 +0900

    Kadai06_4 length

--- pro2/Kadai06_4.java
+++ pro2/Kadai06_4.java
@@ -11,28 +11,28 @@ class Kadai06_4
         try {
             BufferedReader br_f = new BufferedReader(new FileReader(file_name));
             String str;
-            char[] syou;
-            char[] dai;
-            char[] hoka;
+            int syou = 0;
+            int dai = 0;
+            int hoka = 0;
             while ((str = br_f.readLine()) != null) {
                 System.out.println(str);
                 for(int i = 0; i < str.length(); i++) {
                     System.out.println(str.charAt(i));
                     char c = str.charAt(i);
                     if(Character.isLowerCase(c) == true){
-                        syou = str; 
+                        syou += 1; 
                     }else if(Character.isUpperCase(c) == true){
-                        dai = str;
+                        dai += 1;
                     }else{
-                        hoka = str;
+                        hoka += 1;
                     }
                 } 
-                
             } 
             br_f.close();
             System.out.println(str.length + "行ありました");
-            System.out.println("小文字 :" + syou.length);
-            System.out.println("大文字 :" + dai.length);
+            System.out.println("小文字 :" + syou);
+            System.out.println("大文字 :" + dai);
+            System.out.println("その他 :" + hoka);
         } catch (IOException e) {
             System.out.println("入力エラーです");
         }
