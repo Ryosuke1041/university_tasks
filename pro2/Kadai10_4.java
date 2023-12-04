de949d959b36d9827584c8b02e6de8695814d540
Ryosuke1041 <rekyuso11@gmail.com>
2023-12-04 23:41:43 +0900

    getAve_sn

--- pro2/Kadai10_4.java
+++ pro2/Kadai10_4.java
@@ -13,7 +13,7 @@ class Score_4
         s3 = 0;
         total = 0;
         n1++;
-        System.out.printf("＜%3d 番目：インスタンス変数を初期化しました。＞", n1);
+        System.out.printf("＜%3d 番目：インスタンス変数を初期化しました。＞\n", n1);
 	}
 	
 	public Score_4(int s1, int s2)
@@ -22,10 +22,10 @@ class Score_4
 		if(checkScore(s1) == 0 && checkScore(s2) == 0){
 			this.s1 = s1;			this.s2 = s2;
             n2++;
-            System.out.printf("＜%3d つ目の登録", n2);
-			System.out.printf("\n　　科目１：%6d 点\t　　科目２：%6d 点\n", this.s1, this.s2);
-            total_s1 = this.s1;
-            total_s2 = this.s2;
+            System.out.printf("%3d つ目の登録", n2);
+			System.out.printf("　　科目１：%6d 点\t　　科目２：%6d 点\n", this.s1, this.s2);
+            total_s1 += this.s1;
+            total_s2 += this.s2;
 		}
 		else{
 			this.s1 = 0;			this.s2 = 0;
@@ -40,7 +40,12 @@ class Score_4
 	}
 
     public double getAve_sn(int gA){
-
+        if(gA == 1){
+            average = total_s1 / n2;
+        }else if(gA == 2){
+            average = total_s2 / n2;
+        }
+        return average;
     }
 
 	public int getTotal(int n)
@@ -64,25 +69,30 @@ class Score_4
 	}
 }
 
-class Kadai010_4
+class Kadai10_4
 {
 	public static void main(String[] args)
 	{
 		int n; 
  
         Score_4 sub1 = new Score_4( -88, 77); n=2; 
-        System.out.printf(" 合計点：%6d 点 平均点：%6.2f 点\n", sub1.getTotal(n), sub1.getAverage(n)); 
+        System.out.printf(" 合計点：%6d 点 平均点：%6.2f 点\n\n", sub1.getTotal(n), sub1.getAverage(n)); 
         
         Score_4 sub2 = new Score_4( 88, 77); n=2; 
-        System.out.printf(" 合計点：%6d 点 平均点：%6.2f 点\n", sub2.getTotal(n), sub2.getAverage(n)); 
+        System.out.printf(" 合計点：%6d 点 平均点：%6.2f 点\n\n", sub2.getTotal(n), sub2.getAverage(n)); 
         
         Score_4 sub3 = new Score_4( 75, 182); n=2; 
-        System.out.printf(" 合計点：%6d 点 平均点：%6.2f 点\n", sub3.getTotal(n), sub3.getAverage(n)); 
+        System.out.printf(" 合計点：%6d 点 平均点：%6.2f 点\n\n", sub3.getTotal(n), sub3.getAverage(n)); 
         
         Score_4 sub4 = new Score_4( 75, 82); n=2; 
-        System.out.printf(" 合計点：%6d 点 平均点：%6.2f 点\n", sub4.getTotal(n), sub4.getAverage(n)); 
+        System.out.printf(" 合計点：%6d 点 平均点：%6.2f 点\n\n", sub4.getTotal(n), sub4.getAverage(n)); 
         
         System.out.printf("\n"); 
-        //ここに科目１の平均値と科目２の平均値をクラスメソッドの戻り値として取得し表示させるコードを追加しなさい。
-	}
+
+        Score_4 ave1 = new Score_4();//ここに科目１の平均値と科目２の平均値をクラスメソッドの戻り値として取得し表示させるコードを追加しなさい。
+        System.out.printf("科目１の平均点：%6.2f 点", ave1.getAve_sn(1));
+        
+        Score_4 ave2 = new Score_4();//ここに科目１の平均値と科目２の平均値をクラスメソッドの戻り値として取得し表示させるコードを追加しなさい。
+        System.out.printf("科目２の平均点：%6.2f 点", ave2.getAve_sn(2)); 
+    }
 }
