b9497f4121ad9c1aab79cac59edf3100c7c955e0
Ryosuke1041 <rekyuso11@gmail.com>
2023-12-27 19:00:45 +0900

    maru4

---
+++ pro2/Kadai13_3.java
@@ -0,0 +1,44 @@
+// 学籍番号：22H062，氏名：中江亮介，課題番号：課題13_3
+
+
+class Student {
+    public String name, number;
+
+    public Student(String number, String name) {
+        this.number = number;
+        this.name = name;
+    }
+
+    protected Student() {
+        name = "No name";
+        number = "00H000";
+    }
+}
+
+class Score extends Student {
+    private int math;
+    private int english;
+
+    public Score(String number, String name, int math, int english) {
+        super(number, name);
+        this.math = math;
+        this.english = english;
+    }
+
+    private Score() {
+        math = 0;
+        english = 0;
+    }
+
+    public String toString() {
+        return "学籍番号:" + number + ", 名前:" + name + ", 数学:" + math + ", 英語:" + english;
+    }
+}
+
+class Kadai13_3 {
+    public static void main(String[] args) {
+        Score member1 = new Score("22H777", "Taro", 70, 80);
+
+        System.out.println(member1.toString());
+    }
+}
