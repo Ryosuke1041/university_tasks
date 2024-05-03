945f81db62050af7af983b724b378b5234600f21
Ryosuke1041 <rekyuso11@gmail.com>
2024-05-04 03:09:28 +0900

    complete

---
+++ WEBPROG2024/第3回/Robot.py
@@ -0,0 +1,9 @@
+class Robot:
+    def goForward(self, n):
+        print(f'前に{n}歩すすむ ')
+
+    def turnRight(self):
+        print('右を向く')
+
+    def turnLeft(self):
+        print('左を向く')
---
+++ WEBPROG2024/第3回/kadai4_1.py
@@ -0,0 +1,9 @@
+import Robot
+a = Robot.Robot()
+a.goForward(5)
+a.turnRight()
+a.goForward(1)
+a.turnLeft()
+a.goForward(2)
+
+
