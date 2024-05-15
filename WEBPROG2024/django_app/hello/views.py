d64405c876dd6ba98c4936e7a89f3bd8e121c99e
Ryosuke1041 <rekyuso11@gmail.com>
2024-05-15 22:32:22 +0900

    dai6kai

---
+++ WEBPROG2024/django_app/hello/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import HelloForm
+
+def index(request):
+    params = {
+        'title':'Hello',
+        'message':'Your Data',
+        'form': HelloForm(),
+    }
+
+    if request.method == 'POST':
+        params['message'] = '名前: ' + request.POST['name'] + \
+            '<br>メール: ' + request.POST['mail'] + \
+            '<br>年齢: ' + request.POST['age']
+        params['form'] = HelloForm(request.POST)
+
+    return render(request, 'hello/index.html', params)
