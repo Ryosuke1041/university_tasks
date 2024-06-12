fe4b539cd24fa6c5747619702d5dd90d18add1b8
Ryosuke1041 <rekyuso11@gmail.com>
2024-06-12 20:18:21 +0900

    dai10kai

--- WEBPROG2024/django_app/hello/forms.py
+++ WEBPROG2024/django_app/hello/forms.py
@@ -1,4 +1,17 @@
 from django import forms
+from .models import Friend
+
+class FriendForm(forms.ModelForm):
+    class Meta:
+        model = Friend
+        fields = ['name', 'mail', 'gender', 'age', 'birthday']
 
 class HelloForm(forms.Form):
-    gender = forms.fields.ChoiceField(label='GENDER',choices=(('female', 'female'), ('male', 'male')))
+    name = forms.CharField(label='Name')
+    mail = forms.EmailField(label='Email')
+    gender = forms.BooleanField(label='Gender', required=False)
+    age = forms.IntegerField(label='Age')
+    birthday = forms.DateField(label='Birth')
+
+class FindForm(forms.Form):
+    find = forms.CharField(label='Find', required=False)
